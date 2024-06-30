44ec6ed65206f7b11aa9a37f51a94163ebafe049
Milan Ruben Kappen <milan@kappen.name>
2024-06-30 23:20:29 +0200

    feat: configure controle plane command

--- internal/k3s/install/commands/controlplane.go
+++ internal/k3s/install/commands/controlplane.go
@@ -4,9 +4,24 @@ import (
 	"github.com/hetznercloud/hcloud-go/v2/hcloud"
 	"hcloud-k3s-cli/internal/clustercontext"
 	"hcloud-k3s-cli/internal/k3s/install/config"
+	"hcloud-k3s-cli/internal/utils/ip"
 	"hcloud-k3s-cli/internal/utils/yaml"
 	"strings"
 )
+
+func getServer(
+	lb *hcloud.LoadBalancer,
+	controlPlaneNodes []*hcloud.Server,
+) string {
+	address := ""
+	if lb != nil {
+		address = lb.PublicNet.IPv4.IP.String()
+	} else {
+		node := controlPlaneNodes[0]
+		address = ip.FirstAvailable(node)
+	}
+	return "https://" + address + ":6443"
+}
 
 func ControlPlane(
 	ctx clustercontext.ClusterContext,
@@ -14,7 +29,29 @@ func ControlPlane(
 	controlPlaneNodes []*hcloud.Server,
 	node *hcloud.Server,
 ) string {
-	configYaml := yaml.String(config.K3sServerConfig{})
+	nodeIp := ip.FirstAvailable(node)
+
+	configYaml := yaml.String(config.K3sServerConfig{
+		//Server:                   getServer(lb, controlPlaneNodes),
+		NodeName:                 node.Name,
+		Token:                    ctx.Credentials.K3sToken,
+		ClusterInit:              true,
+		DisableCloudController:   true,
+		DisableKubeProxy:         false,
+		DisableComponents:        []string{},
+		KubeletArg:               []string{},
+		KubeControllerManagerArg: []string{},
+		FlannelIface:             "eth1",
+		NodeIP:                   []string{nodeIp},
+		AdvertiseAddress:         nodeIp,
+		NodeTaint:                []string{},
+		NodeLabel:                []string{},
+		ClusterCIDR:              "10.42.0.0/16",
+		ServiceCIDR:              "10.43.0.0/16",
+		ClusterDNS:               "10.43.0.10",
+		SELinux:                  true,
+		WriteKubeconfigMode:      "0644",
+	})
 	commandArr := []string{
 		PreInstallCommand(configYaml),
 		K3sCommand(ctx),
