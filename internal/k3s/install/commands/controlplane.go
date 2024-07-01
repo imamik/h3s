5aa1991c4dfb62e199f02c3828374d3465db1e7a
Milan Ruben Kappen <milan@kappen.name>
2024-07-01 08:12:15 +0200

    feat: define default config

--- internal/k3s/install/commands/controlplane.go
+++ internal/k3s/install/commands/controlplane.go
@@ -11,16 +11,31 @@ import (
 
 func getServer(
 	lb *hcloud.LoadBalancer,
-	controlPlaneNodes []*hcloud.Server,
+	node *hcloud.Server,
 ) string {
 	address := ""
 	if lb != nil {
 		address = lb.PublicNet.IPv4.IP.String()
 	} else {
-		node := controlPlaneNodes[0]
 		address = ip.FirstAvailable(node)
 	}
 	return "https://" + address + ":6443"
+}
+
+func getTlsSan(
+	lb *hcloud.LoadBalancer,
+	controlPlaneNodes []*hcloud.Server,
+) []string {
+	var tlsSan []string
+	if lb != nil {
+		tlsSan = append(tlsSan, lb.PublicNet.IPv4.IP.String())
+		tlsSan = append(tlsSan, lb.PublicNet.IPv6.IP.String())
+	} else {
+		for _, node := range controlPlaneNodes {
+			tlsSan = append(tlsSan, ip.FirstAvailable(node))
+		}
+	}
+	return tlsSan
 }
 
 func ControlPlane(
@@ -29,28 +44,56 @@ func ControlPlane(
 	controlPlaneNodes []*hcloud.Server,
 	node *hcloud.Server,
 ) string {
-	nodeIp := ip.FirstAvailable(node)
+	nodeIp := node.PrivateNet[0].IP.String()
 
 	configYaml := yaml.String(config.K3sServerConfig{
-		//Server:                   getServer(lb, controlPlaneNodes),
-		NodeName:                 node.Name,
-		Token:                    ctx.Credentials.K3sToken,
-		ClusterInit:              true,
-		DisableCloudController:   true,
-		DisableKubeProxy:         false,
-		DisableComponents:        []string{},
-		KubeletArg:               []string{},
-		KubeControllerManagerArg: []string{},
-		FlannelIface:             "eth1",
-		NodeIP:                   []string{nodeIp},
-		AdvertiseAddress:         nodeIp,
-		NodeTaint:                []string{},
-		NodeLabel:                []string{},
-		ClusterCIDR:              "10.42.0.0/16",
-		ServiceCIDR:              "10.43.0.0/16",
-		ClusterDNS:               "10.43.0.10",
-		SELinux:                  true,
-		WriteKubeconfigMode:      "0644",
+		// Node
+		NodeName:  node.Name,
+		NodeLabel: []string{},
+
+		// Server
+		Server: getServer(lb, node),
+		Token:  ctx.Credentials.K3sToken,
+
+		// Cluster
+		ClusterInit: node.ID == controlPlaneNodes[0].ID,
+		NodeTaint:   []string{},
+
+		// Disabled Functionality
+		DisableCloudController: true,
+		DisableKubeProxy:       false,
+		DisableComponents: []string{
+			"local-storage",
+			"servicelb",
+			"traefik",
+		},
+
+		// Kube
+		KubeletArg: []string{
+			"cloud-provider=external",
+			"volume-plugin-dir=/var/lib/kubelet/volumeplugins",
+		},
+		KubeControllerManagerArg: []string{
+			"flex-volume-plugin-dir=/var/lib/kubelet/volumeplugins",
+		},
+
+		// Network
+		FlannelIface:     "eth1",
+		NodeIP:           []string{nodeIp},
+		AdvertiseAddress: nodeIp,
+		ClusterCIDR:      "10.42.0.0/16",
+		ServiceCIDR:      "10.43.0.0/16",
+		ClusterDNS:       "10.43.0.10",
+
+		// CNI
+		DisableNetworkPolicy: false,
+		FlannelBackend:       "vxlan",
+
+		// System
+		SELinux:             true,
+		WriteKubeconfigMode: "0644",
+
+		TLSSAN: getTlsSan(lb, controlPlaneNodes),
 	})
 	commandArr := []string{
 		PreInstallCommand(configYaml),
