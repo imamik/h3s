42230b2e882efaca73daa731f377314bda16c78c
Milan Ruben Kappen <milan@kappen.name>
2024-07-12 09:05:08 +0200

    refactor: parallelize cluster destroy calls

--- internal/resources/cluster/destroy.go
+++ internal/resources/cluster/destroy.go
@@ -10,17 +10,38 @@ import (
 	"hcloud-k3s-cli/internal/resources/sshkey"
 	"hcloud-k3s-cli/internal/utils/file"
 	"hcloud-k3s-cli/internal/utils/logger"
+	"sync"
 )
+
+func deleteResource(
+	ctx clustercontext.ClusterContext,
+	wg *sync.WaitGroup,
+	deleteFunc func(clustercontext.ClusterContext),
+) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		deleteFunc(ctx)
+	}()
+}
 
 func Destroy(ctx clustercontext.ClusterContext) {
 	logger.LogResourceEvent(logger.Cluster, logger.Delete, ctx.Config.Name, logger.Initialized)
 
-	gateway.Delete(ctx)
-	loadbalancers.Delete(ctx)
-	pool.Delete(ctx)
-	network.Delete(ctx)
-	sshkey.Delete(ctx)
-	dns.Delete(ctx)
+	var wg sync.WaitGroup
+
+	deleteResource(ctx, &wg, gateway.Delete)
+	deleteResource(ctx, &wg, loadbalancers.Delete)
+	deleteResource(ctx, &wg, pool.Delete)
+	deleteResource(ctx, &wg, dns.Delete)
+
+	wg.Wait()             // Wait for all deletions to complete
+	wg = sync.WaitGroup{} // Reset the wait group
+
+	deleteResource(ctx, &wg, network.Delete)
+	deleteResource(ctx, &wg, sshkey.Delete)
+
+	wg.Wait() // Wait for all deletions to complete
 
 	file.Delete("./k3s.yaml")
 
--- internal/resources/pool/delete.go
+++ internal/resources/pool/delete.go
@@ -6,23 +6,42 @@ import (
 	"hcloud-k3s-cli/internal/resources/pool/node"
 	"hcloud-k3s-cli/internal/resources/pool/placementgroup"
 	"hcloud-k3s-cli/internal/utils/logger"
+	"sync"
 )
 
 func Delete(ctx clustercontext.ClusterContext) {
+	var wg sync.WaitGroup
+
 	// Delete control plane pool
-	deletePool(ctx, ctx.Config.ControlPlane.Pool)
+	wg.Add(1)
+	go func() {
+		deletePool(ctx, ctx.Config.ControlPlane.Pool)
+		wg.Done()
+	}()
 
 	// Delete worker pools
 	for _, pool := range ctx.Config.WorkerPools {
-		deletePool(ctx, pool)
+		wg.Add(1)
+		go func(pool config.NodePool) {
+			deletePool(ctx, pool)
+			wg.Done()
+		}(pool)
 	}
+
+	wg.Wait()
 }
 
 func deletePool(ctx clustercontext.ClusterContext, pool config.NodePool) {
 	logger.LogResourceEvent(logger.Pool, logger.Delete, ctx.GetName(pool.Name), logger.Initialized)
+	var wg sync.WaitGroup
 	for i := 0; i < pool.Nodes; i++ {
-		node.Delete(ctx, pool, i)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			node.Delete(ctx, pool, i)
+		}(i)
 	}
+	wg.Wait()
 	placementgroup.Delete(ctx, pool)
 	logger.LogResourceEvent(logger.Pool, logger.Delete, ctx.GetName(pool.Name), logger.Success)
 }
