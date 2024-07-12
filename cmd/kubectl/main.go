354d766ef3a32b50bb87b6bea66e1e31587014f3
Milan Ruben Kappen <milan.kappen@deepup.de>
2024-07-12 12:39:43 +0200

    feat: resolve file before kubectl apply

--- cmd/kubectl/main.go
+++ cmd/kubectl/main.go
@@ -2,7 +2,6 @@ package kubectl
 
 import (
 	"github.com/spf13/cobra"
-	"hcloud-k3s-cli/internal/clustercontext"
 	"hcloud-k3s-cli/internal/kubectl"
 )
 
@@ -11,25 +10,6 @@ var Kubectl = &cobra.Command{
 	Short:              "Proxy kubectl commands via ssh to the Kubernetes API server",
 	DisableFlagParsing: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		useSsh := false
-		var filteredArgs []string
-
-		for _, arg := range args {
-			if arg == "--ssh" {
-				useSsh = true
-			} else {
-				filteredArgs = append(filteredArgs, arg)
-			}
-		}
-
-		if useSsh {
-			ctx := clustercontext.Context()
-			err := kubectl.SSH(ctx, filteredArgs)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			kubectl.Kubectl(filteredArgs)
-		}
+		kubectl.Execute(args)
 	},
 }
---
+++ internal/kubectl/execute.go
@@ -0,0 +1,41 @@
+package kubectl
+
+import (
+	"hcloud-k3s-cli/internal/clustercontext"
+	"hcloud-k3s-cli/internal/utils/file"
+)
+
+func Execute(args []string) {
+	useSsh := false
+	var filteredArgs []string
+
+	for _, arg := range args {
+		if arg == "--ssh" {
+			useSsh = true
+		} else {
+			filteredArgs = append(filteredArgs, arg)
+		}
+	}
+
+	// iterate over all filteredArgs
+	for i, arg := range filteredArgs {
+		if arg == "-f" || arg == "--filename" {
+			// replace the filename with the content of the file
+			content, err := file.Load(filteredArgs[i+1])
+			if err != nil {
+				panic(err)
+			}
+			filteredArgs[i+1] = "- <<EOF\n" + string(content) + "\nEOF"
+		}
+	}
+
+	if useSsh {
+		ctx := clustercontext.Context()
+		err := SSH(ctx, filteredArgs)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		Kubectl(filteredArgs)
+	}
+}
