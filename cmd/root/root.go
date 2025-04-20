72dcbc37377b654b288b273762a854bd13658417
Milan Kappen <milan.kappen@iu.org>
2025-04-20 09:33:14 +0100

    fix: Add version flag and register CLI subcommands in root command

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -7,7 +7,14 @@ import (
 
 	"github.com/spf13/cobra"
 
+	cmdcreate "h3s/cmd/create"
+	cmddestroy "h3s/cmd/destroy"
+	cmdget "h3s/cmd/get"
+	cmdinstall "h3s/cmd/install"
+	cmdkubectl "h3s/cmd/kubectl"
+	cmdssh "h3s/cmd/ssh"
 	cmdversion "h3s/cmd/version"
+	internalversion "h3s/internal/version"
 )
 
 // Cmd is the root command for the h3s CLI
@@ -21,15 +28,33 @@ func runRoot(cmd *cobra.Command, _ []string) {
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 func Execute() {
+	var showVersion bool
 	Cmd = &cobra.Command{
 		Use:   "h3s",
 		Short: "A CLI to setup k3s Kubernetes resources on Hetzner Cloud",
 		Long: `h3s simplifies the creation, management, and deletion of K3s clusters
 hosted on Hetzner Cloud infrastructure.`,
-		Run: runRoot,
+		Run: func(cmd *cobra.Command, args []string) {
+			if showVersion {
+				info := internalversion.GetBuildInfo()
+				cmd.Printf("h3s version %s\n", info.Version)
+				cmd.Printf("Commit: %s\n", info.Commit)
+				cmd.Printf("Go version: %s\n", info.GoVersion)
+				return
+			}
+			runRoot(cmd, args)
+		},
 	}
 
+	Cmd.PersistentFlags().BoolVarP(&showVersion, "version", "v", false, "Show version information and exit")
+
 	Cmd.AddCommand(cmdversion.Cmd)
+	Cmd.AddCommand(cmdcreate.Cmd)
+	Cmd.AddCommand(cmddestroy.Cmd)
+	Cmd.AddCommand(cmdget.Cmd)
+	Cmd.AddCommand(cmdinstall.Cmd)
+	Cmd.AddCommand(cmdkubectl.Cmd)
+	Cmd.AddCommand(cmdssh.Cmd)
 
 	if err := Cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
